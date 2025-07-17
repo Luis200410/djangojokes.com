cf59daca49c50adb9b4f44a6ea227ecee0d3aded
Luis Castaneda <luiseduardocastanedaruiz@gmail.com>
2025-07-17 09:21:59 -0400

    Add CRUD functionality for jokes: implement create, update, and delete views; update URLs and templates accordingly

--- jokes/urls.py
+++ jokes/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 from .views import JokeListView, JokeDetailView
 
+from .views import (
+    JokeCreateView, JokeDetailView, JokeListView, JokeUpdateView, JokeDeleteView
+)
+
 app_name = 'jokes'
 urlpatterns = [
 path('', JokeListView.as_view(), name='list'),
 path('joke/<int:pk>/', JokeDetailView.as_view(), name='detail'),
+path('joke/<int:pk>/update/', JokeUpdateView.as_view(), name='update'),
+path('joke/create/', JokeCreateView.as_view(), name='create'),
+path('joke/<int:pk>/delete/', JokeDeleteView.as_view(), name='remove'),
 ]
+
