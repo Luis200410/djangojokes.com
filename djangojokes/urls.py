ed6fa6157c8731c0875c76488401fb87b19eeff9
Luis Castaneda <luiseduardocastanedaruiz@gmail.com>
2025-08-13 09:42:13 -0400

    Added media files rooting and partial storage

--- djangojokes/urls.py
+++ djangojokes/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     # Admin
     path('admin/doc/', include('django.contrib.admindocs.urls')),
@@ -14,4 +17,5 @@ urlpatterns = [
     path('jokes/', include('jokes.urls')),
     path('', include('pages.urls')),
     path('account/', include('users.urls')),
-]
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
