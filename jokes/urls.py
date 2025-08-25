4850a58b94ab91853a8c1c549d7d2bd26c848ee7
Luis Castaneda <luiseduardocastanedaruiz@gmail.com>
2025-08-25 10:07:15 -0400

    Filtering Results by Category, Tag, or Creator, so we modified the views on the joke and modified also templates

--- jokes/urls.py
+++ jokes/urls.py
@@ -11,6 +11,9 @@ urlpatterns = [
     path('joke/<slug>/delete/', JokeDeleteView.as_view(), name='delete'),
     path('joke/create/', JokeCreateView.as_view(), name='create'),
     path('joke/<slug>/', JokeDetailView.as_view(), name='detail'),
+    path('category/<slug>/', JokeListView.as_view(), name='category'),
+    path('tag/<slug>/', JokeListView.as_view(), name='tag'),
+    path('creator/<username>/', JokeListView.as_view(), name='creator'),
     path('joke/<slug>/vote/', vote, name='ajax-vote'),
     path('', JokeListView.as_view(), name='list'),
 ]
