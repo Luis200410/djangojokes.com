b5a377fade33ddfe38a469b5aca230354093d0cd
Luis Castaneda <luiseduardocastanedaruiz@gmail.com>
2025-09-02 10:03:56 -0400

    added a link in the user form to the password-change form, and unreggistring three apps for authenticating that weren't beign used

--- users/admin.py
+++ users/admin.py
@@ -3,6 +3,9 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 from common.admin import DjangoJokesAdmin
 from common.utils.admin import append_fields, move_fields, remove_fields
+from django.utils.safestring import mark_safe
+from django.urls import reverse
+from allauth.socialaccount.models import SocialApp, SocialAccount, SocialToken
 
 
 CustomUser = get_user_model()
@@ -14,6 +17,7 @@ class CustomUserAdmin(DjangoJokesAdmin, UserAdmin):
     # List Attributes
     list_display = UserAdmin.list_display + ('is_superuser',)
     list_display_links = ('username', 'email', 'first_name', 'last_name')
+    readonly_fields = ['password_form']  # Display link to password change form.
 
     # Fields for editing existing user.
     new_fields = ('dob', 'avatar')
@@ -23,6 +27,7 @@ class CustomUserAdmin(DjangoJokesAdmin, UserAdmin):
     move_fields(UserAdmin.fieldsets, 'Personal info', None, ('email',))
     # Remove password field.
     remove_fields(UserAdmin.fieldsets, None, ('password',))
+    append_fields(UserAdmin.fieldsets, None, ('password_form',))
 
     # Fields for adding new user.
     new_fields = ('email', )
@@ -37,3 +42,11 @@ class CustomUserAdmin(DjangoJokesAdmin, UserAdmin):
     def get_form(self, request, obj=None, **kwargs):
         self.save_on_top = obj is not None
         return super().get_form(request, obj, **kwargs)
+    
+    def password_form(self, obj):
+        url = reverse('admin:auth_user_password_change', args=[obj.pk])
+        return mark_safe(f'<a href="{url}">Change password</a>')
+    
+admin.site.unregister(SocialApp)
+admin.site.unregister(SocialAccount)
+admin.site.unregister(SocialToken)
